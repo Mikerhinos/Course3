b86ee171baf4cfef966ed35a26f9009f28897b68
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-12 14:56:43 +0200

    fin Course3 Signed-off-by:Michael Dufour <mikerhinosfr@gmail.com>

--- course3/course3.py
+++ course3/course3.py
@@ -12,8 +12,24 @@ def enter_data():
     c.execute("INSERT into example VALUES ('Python',3.5,'Expert')")
     conn.commit()
     
+def enter_dynamic_data():
+    lang = input("What language ? : ")
+    version = float(input("What version ? : "))
+    skill = input("What skill level ? : ")
+    c.execute("INSERT into example (Language, Verions, Skill) VALUES (?,?,?)", (lang,version,skill))
+    conn.commit()
     
-enter_data()
+def read_from_database():
+    what_skill = input("What skill ? : ")
+    what_language = input("What language ? : ")
+    #Alternative with concatenate way :
+    #sql = "SELECT * FROM example WHERE Skill = '"+what_skill+"' AND Language = '"+what_language+"'"
+    #for row in c.execute(sql):
+    sql = "SELECT * FROM example WHERE Skill = ? AND Language = ?"
+    for row in c.execute(sql,[(what_skill),(what_language)]):
+        print(row)
+    
+read_from_database()
 
 
-#conn.close()
+conn.close()
