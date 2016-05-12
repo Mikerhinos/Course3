bd936d8a1227bc9d831738d1b2956688ff8ecf69
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-12 11:42:03 +0200

    database insert values from course3 Signed-off-by:Michael Dufour <mikerhinosfr@gmail.com>

--- course3/course3.py
+++ course3/course3.py
@@ -1,4 +1,19 @@
 import sqlite3
 
 conn = sqlite3.connect("tutorial.db")
-c = conn.cursor
+c = conn.cursor()
+
+def create_table():
+    c.execute("CREATE TABLE example(Language VARCHAR, Verions REAL, Skill TEXT)")
+    
+def enter_data():
+    c.execute("INSERT into example VALUES ('Python',2.7,'Beginner')")
+    c.execute("INSERT into example VALUES ('Python',3.3,'Intermediate')")
+    c.execute("INSERT into example VALUES ('Python',3.5,'Expert')")
+    conn.commit()
+    
+    
+enter_data()
+
+
+#conn.close()
